f8c8517581f81d6c5fed0f820bac2fa6f9338b96
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2024-11-13 12:10:44 -0300

    refactor App component to simplify structure by removing unnecessary elements

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import { Outlet } from "react-router-dom";
 import "./assets/styles/main.scss";
 
 function App() {
-  return (
-    <div>
-      <h1>Meu Site</h1>
-      <Outlet />
-    </div>
-  );
+  return <Outlet />;
 }
 
 export default App;
